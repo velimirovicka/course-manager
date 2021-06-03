d7306a64291c7807550099a693a39be62242fd00
velimirovicka <velimirovicka@gmail.com>
2021-06-03 15:11:37 +0200

    ..

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,56 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	@Autowired
+	private TopicRepository topicRepository;
+	
+	/* private List<Topic> topics =new ArrayList<>(Arrays.asList(
+			new Topic("spring","Spring Framework", "Spring Framework Desc"),
+			new Topic("java"," Core Java", "Core java Desc"),
+			new Topic("JS"," JavaScript", "JS Desc")
+			));
+	*/
+	public List<Topic> getAllTopics(){
+		//return topics;
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		//return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id).get();
+	}
+
+	public void addToppic(Topic topic) {
+		topicRepository.save(topic);
+		
+	}
+
+	public void updateToppic(String id, Topic topic) {
+	/*	for(int i=0; i<topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			} 
+		} */
+		
+		topicRepository.save(topic);
+		
+	}
+
+	public void deleteTopic(String id) {
+		//topics.removeIf(t->t.getId().equals(id));
+		topicRepository.deleteById(id);
+	}
+}
